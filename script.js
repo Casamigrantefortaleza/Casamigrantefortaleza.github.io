568b14620f67b5126cdf8b93cd97c4a8436ddb0d
Rodrigo Mendes <rodrigocesarsm@gmail.com>
2023-11-17 16:52:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.carousel').slick({
+      arrows: false, // Adicione setas de navegação (opcional)
+      dots: true,   // Adicione indicadores (pontos) de navegação (opcional)
+      autoplay: true, // Inicie a reprodução automática (opcional)
+      autoplaySpeed: 3000, // Defina a velocidade da reprodução automática (opcional)
+      infinite: true, // Repetição infinita (opcional)
+      speed: 500, // Velocidade da animação (opcional)
+      slidesToShow: 1, // Quantidade de slides visíveis (opcional)
+    });
+  });
+  
